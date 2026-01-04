b3ac3efe966f3849f9b6c88320b136d8b05d1357
Anastasiya Vusik <you@example.com>
2026-01-04 23:26:21 +0300

    Profiles were added

---
+++ src/main/java/com/example/data511_monitoring/Data511MonitoringApplication.java
@@ -0,0 +1,16 @@
+package com.example.data511_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data511MonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data511MonitoringApplication.class, args);
+		IO.println("\n\nHello World from Data511 Monitoring Application!");
+				System.out.println("\n\nHello World from Data511 Monitoring Application1!");
+
+	}
+
+}
---
+++ src/test/java/com/example/data511_monitoring/Data511MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data511_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data511MonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
